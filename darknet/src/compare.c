282d746bbac604de0ff4729499f6e1f4dcb8c1a2
Mohan Abhyas <stylishabhyas007@gmail.com>
2018-12-05 02:12:15 +0530

    try

--- darknet/src/compare.c
+++ darknet/src/compare.c
@@ -58,14 +58,14 @@ void train_compare(char *cfgfile, char *weightfile)
         free_data(train);
         if(i%100 == 0){
             char buff[256];
-            sprintf(buff, "%s/%s_%d_minor_%d.weights",backup_directory,base, epoch, i);
+            sprintf(buff, "%s/%s_%d_minor_%d.weights","/content/yolov3/darknet/gdrive/My Drive",base, epoch, i);
             save_weights(net, buff);
         }
         if(*net.seen/N > epoch){
             epoch = *net.seen/N;
             i = 0;
             char buff[256];
-            sprintf(buff, "%s/%s_%d.weights",backup_directory,base, epoch);
+            sprintf(buff, "%s/%s_%d.weights","/content/yolov3/darknet/gdrive/My Drive",base, epoch);
             save_weights(net, buff);
             if(epoch%22 == 0) net.learning_rate *= .1;
         }
