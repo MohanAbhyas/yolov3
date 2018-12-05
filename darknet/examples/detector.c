730b94a570f2e8da164e17046505b742b8520f62
Mohan Abhyas <stylishabhyas007@gmail.com>
2018-12-05 13:55:53 +0530

    changed detector.c

--- darknet/examples/detector.c
+++ darknet/examples/detector.c
@@ -132,15 +132,15 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
-            sprintf(buff, "%s/%s.backup", backup_directory, base);
+            sprintf(buff, "%s/%s.backup", "/content/yolov3/darknet/gdrive/My\ Drive", base);
             save_weights(net, buff);
         }
-        if(i%10000==0 || (i < 1000 && i%100 == 0)){
+        if(i%10000==0 || (i < 1000 && i%10 == 0)){
 #ifdef GPU
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
-            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
+            sprintf(buff, "%s/%s_%d.weights", "/content/yolov3/darknet/gdrive/My\ Drive", base, i);
             save_weights(net, buff);
         }
         free_data(train);
