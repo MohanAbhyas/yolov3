04c834bb390590cf02ea1f684b99f035b01a7e62
Mohan Abhyas <stylishabhyas007@gmail.com>
2019-03-10 01:03:34 +0530

    saving in gdrive

--- darknet/examples/detector.c
+++ darknet/examples/detector.c
@@ -132,7 +132,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
-            sprintf(buff, "%s/%s.backup", backup_directory, base);
+            sprintf(buff, "%s/%s.backup", "/content/yolov3/darknet/gdrive/My\ Drive", base);
             save_weights(net, buff);
         }
         if(i%10000==0 || (i < 1000 && i%100 == 0) || (i < 10000 && i%1000 == 0)){
@@ -140,7 +140,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
             char buff[256];
-            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
+            sprintf(buff, "%s/%s_%d.weights", "/content/yolov3/darknet/gdrive/My\ Drive", base, i);
             save_weights(net, buff);
         }
         free_data(train);
